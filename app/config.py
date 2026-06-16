4465c97402f86c650306ed35e7bb2f57d49651db
Ahad-theDev <ahad04pk@gmail.com>
2026-06-16 22:27:06 +0500

    config.py changed

--- app/config.py
+++ app/config.py
@@ -1,16 +1,29 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
 
 class Settings(BaseSettings):
-    database_url: str | None = None
-    # database_hostname:str
-    # database_port:int
-    # database_password:str
-    # database_name:str
-    # database_username:str
-    secret_key:str
-    algorithm:str
-    access_token_expire_minutes:int
+    database_hostname: str = ""
+    database_port: int = 5432
+    database_password: str = ""
+    database_name: str = ""
+    database_username: str = ""
+    database_url: Optional[str] = None  # Render provides this
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+# class Settings(BaseSettings):
+#     database_url: str | None = None
+#     # database_hostname:str
+#     # database_port:int
+#     # database_password:str
+#     # database_name:str
+#     # database_username:str
+#     secret_key:str
+#     algorithm:str
+#     access_token_expire_minutes:int
     
-    # model_config = SettingsConfigDict(env_file=".env")
+#     # model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()

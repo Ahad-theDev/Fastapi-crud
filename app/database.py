f909ab4f1b6610f074ac352e7f6e6bd76199860b
Ahad-theDev <ahad04pk@gmail.com>
2026-06-16 22:39:35 +0500

    databse.py changes

--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from .config import settings
 
-# database_url=f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 if settings.database_url:
-    database_url = settings.database_url
+    # Render gives postgresql:// but psycopg3 needs postgresql+psycopg://
+    database_url = settings.database_url.replace(
+        "postgresql://", "postgresql+psycopg://", 1
+    )
 else:
     database_url = (
         f"postgresql+psycopg://"
@@ -13,25 +15,13 @@ else:
         f"{settings.database_hostname}/"
         f"{settings.database_name}"
     )
-# database_url = (
-#     f"postgresql+psycopg://{settings.database_username}:"
-#     f"{settings.database_password}@"
-#     f"{settings.database_hostname}:"
-#     f"{settings.database_port}/"
-#     f"{settings.database_name}"
-# )
 
 engine = create_engine(database_url)
-
 Base = declarative_base()
-
-SessionLocal =  sessionmaker(
-    bind=engine,
-    autoflush=False
-)
+SessionLocal = sessionmaker(bind=engine, autoflush=False)
 
 def get_db():
-    db=SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
